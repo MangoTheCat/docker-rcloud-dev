e8f4c3057892c96af544451445ff65f26c1fa1f2
Doug Ashton <douglas.j.ashton@gmail.com>
2017-06-26 17:14:45 +0100

    change init

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sudo ln -s /save/mathjax /data/rcloud/htdocs/mathjax
+sudo ln -s /data/rcloud.solr /data/rcloud/rcloud.packages/rcloud.solr
+sudo cp /save/rcloud.conf /data/rcloud/conf/
 sudo chown -R rcloud:rcloud /data/rcloud
 
 sudo su - rcloud -c "cd /data/rcloud && scripts/build.sh --all"
-sudo su - rcloud -c "cd /data/rcloud && git apply docker/domainCookie.patch"
 
 sudo su - rcloud -c "ROOT=/data/rcloud /data/rcloud/conf/start"
 sudo su - rcloud -c "/data/rcloud/docker/ulogd/ulogd /data/rcloud/run/ulog"
--- run_commands.sh
+++ run_commands.sh
@@ -7,4 +7,5 @@ docker run -d -p 8080:8080 --link solr:solr rcl0ud/rcloud
 # With the mounting
 docker run -it -p 8080:8080 -v rcloud:/data/rcloud -v rcloud.support:/data/rcloud/rcloud.packages/rcloud.support rcdev
 
+docker run -it -p 8080:8080 --rm -v /c/Users/dashton.Mango/Documents/Projects/ATT/rcloud:/data/rcloud -v /c/Users/dashton.Mango/Documents/Projects/ATT/rcloud.solr:/data/rcloud.solr rcdev
 # Access solr via solr.url: http://solr:8983/solr/rcloudnotebooks
